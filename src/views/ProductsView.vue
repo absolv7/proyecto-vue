2463366ad6504e9a5777fb7c4823891228b6fccd
absolvt <cabraltonanez@gmail.com>
2022-04-27 15:55:50 -0300

    desestructuración

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,11 +2,11 @@
 	<div>
 		<h1>Products</h1>
 		<div class="products">
-			<div v-for="item in items" :key="item.nombre">
+			<div v-for="{ nombre, precio, imagen } in items" :key="nombre">
 				<ItemVue
-					:nombre="item.nombre"
-					:precio="item.precio"
-					:imagen="item.imagen"
+					:nombre="nombre"
+					:precio="precio"
+					:imagen="imagen"
 				></ItemVue>
 			</div>
 		</div>
@@ -15,27 +15,30 @@
 
 <script>
 import ItemVue from '../components/Item.vue';
+
+const items = [
+	{
+		nombre: 'HyperX Pulsefire Dart',
+		precio: 2500,
+		imagen: 'https://http2.mlstatic.com/D_NQ_NP_601015-MLA41817790074_052020-O.webp',
+	},
+	{
+		nombre: 'HyperX Pulsefire',
+		precio: 5000,
+		imagen: 'https://http2.mlstatic.com/D_NQ_NP_835019-MLA32158087459_092019-O.webp',
+	},
+	{
+		nombre: 'Logitech Lightspeed G703',
+		precio: 3000,
+		imagen: 'https://http2.mlstatic.com/D_NQ_NP_920865-MLA41632030645_052020-O.webp',
+	},
+];
+
 export default {
 	name: 'Products',
 	data() {
 		return {
-			items: [
-				{
-					nombre: 'HyperX Pulsefire Dart',
-					precio: 2500,
-					imagen: 'https://http2.mlstatic.com/D_NQ_NP_601015-MLA41817790074_052020-O.webp',
-				},
-				{
-					nombre: 'HyperX Pulsefire',
-					precio: 5000,
-					imagen: 'https://http2.mlstatic.com/D_NQ_NP_835019-MLA32158087459_092019-O.webp',
-				},
-				{
-					nombre: 'Logitech Lightspeed G703',
-					precio: 3000,
-					imagen: 'https://http2.mlstatic.com/D_NQ_NP_920865-MLA41632030645_052020-O.webp',
-				},
-			],
+			items,
 		};
 	},
 	components: { ItemVue },
@@ -43,7 +46,6 @@ export default {
 </script>
 
 <style scoped>
-
 h1 {
 	font-style: oblique;
 	text-decoration: underline;
