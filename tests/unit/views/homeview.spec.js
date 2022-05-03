28a417e15f1dc95a57b7ed303c4e4ffbd5d02cf1
Gabriel Cabral Toñanez <cabraltonanez@gmail.com>
2022-05-03 14:58:20 -0300

    test div class home

--- tests/unit/views/homeview.spec.js
+++ tests/unit/views/homeview.spec.js
@@ -2,15 +2,15 @@ import { shallowMount } from "@vue/test-utils";
 import HomeView from "@/views/HomeView";
 
 describe("Componente HomeView", () => {
-  test("La clase debe ser HOME", () => {
-    const wrapper = shallowMount(HomeView);
 
-      // Capturo las clases del div, en la variable clases
-    const clases = wrapper.find('div').classes()
+    test("Debe existir un div con la clase home", () => {
+        const wrapper = shallowMount(HomeView);
 
-      // Espero que "clases" contenga "home"
-    expect(clases).toContain("home")
+        // Capturo las clases del div, en la variable clases
+        const clases = wrapper.find('div').classes()
 
+        // Espero que "clases" contenga "home"
+        expect(clases).toContain("home")
 
-  });
+    });
 });
