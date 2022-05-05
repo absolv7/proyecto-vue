151f07d0448b42f20276b66b763259ebe50d6ea0
Gabriel Cabral Toñanez <cabraltonanez@gmail.com>
2022-05-05 15:09:56 -0300

    precio total

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -1,62 +1,78 @@
 <template>
-    <div class="carrito" v-b-modal.carrito-mod v-b-tooltip.hover.righttop title="Click para ver mi carrito">
-        <b-icon class="carrito-icon" icon="cart"></b-icon>
+	<div
+		class="carrito"
+		v-b-modal.carrito-mod
+		v-b-tooltip.hover.righttop
+		title="Click para ver mi carrito"
+	>
+		<b-icon class="carrito-icon" icon="cart"></b-icon>
 		<h2>:</h2>
-        <h2>{{ productos.length }}</h2>
-        <b-modal id="carrito-mod" title="Mi Carrito" scrollable
-            >Total de productos: {{ productos.length }}
-            <ol>
-                <li v-for="({ nombre, precio }, index) in productos" :key="index">
-                    <p>* {{ nombre }} = ${{precio}}</p>
-                    <b-button @click="$emit('eliminarCarrito', { index })">
-                        <b-icon icon="cart-x-fill"></b-icon>
-                    </b-button>
-                </li>
-            </ol>
-        </b-modal>
-    </div>
+		<h2>{{ productos.length }}</h2>
+		<b-modal id="carrito-mod" title="Mi Carrito" scrollable
+			>Cantidad de productos: {{ productos.length }}
+
+			<ol>
+				<li
+					v-for="({ nombre, precio }, index) in productos"
+					:key="index"
+				>
+					<p>* {{ nombre }} = ${{ precio }}</p>
+					<b-button @click="$emit('eliminarCarrito', { index })">
+						<b-icon icon="cart-x-fill"></b-icon>
+					</b-button>
+				</li>
+			</ol>
+			<p>
+				Total ${{
+					productos.reduce(
+						(total, producto) => total + producto.precio,
+						0
+					)
+				}}
+			</p>
+		</b-modal>
+	</div>
 </template>
 
 <script>
 export default {
-    name: "Carrito",
-    props: {
-        nombre: String,
-        precio: Number,
-        imagen: String,
-        productos: Array,
-    },
+	name: 'Carrito',
+	props: {
+		nombre: String,
+		precio: Number,
+		imagen: String,
+		productos: Array,
+	},
 };
 </script>
 
 <style scoped>
 .carrito-icon {
-    font-size: 25px;
+	font-size: 25px;
 }
 
 .carrito {
-    display: flex !important;
-    width: 110px !important;
-    background-color: aquamarine;
-    justify-content: space-evenly !important;
-    align-items: baseline !important;
-    gap: 5px;
-    border: 2px solid black;
-    width: fit-content;
-    padding: 8px;
-    border-radius: 5px;
+	display: flex !important;
+	width: 110px !important;
+	background-color: aquamarine;
+	justify-content: space-evenly !important;
+	align-items: baseline !important;
+	gap: 5px;
+	border: 2px solid black;
+	width: fit-content;
+	padding: 8px;
+	border-radius: 5px;
 }
 
 li {
-    display: flex;
-    align-items: baseline;
+	display: flex;
+	align-items: baseline;
 	justify-content: space-between;
-
 }
 
 .precio-producto {
-    margin-left: 10px;
-    margin-right: 30px;
-    text-decoration: underline;
+	margin-left: 10px;
+	margin-right: 30px;
+	text-decoration: underline;
 }
 </style>
